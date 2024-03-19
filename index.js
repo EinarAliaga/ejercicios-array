1c2c6611b7862b262146f00259e0b91612e94ad1
servo pcerda <fernandonando98@live.com.mx>
2024-03-18 18:09:45 -0600

    pre carga de json

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-console.log('Hola mundo');
-
-//Botones
-const createButton = document.getElementById('createButton');
-const deleteButton = document.getElementById('deleteButton');
-//eventos
-//1er param string de evento
-createButton.addEventListener('click', () => {
-  console.log('Click');
-});
-
-//Crear etiqueta <p>
-const parrafo = document.createElement('p');
-//Agrego texto a mi etiqueta p
-parrafo.textContent = 'Texto dentro del párrafo';
-
-//Buscar div con id contenedor
-const contenedor = document.getElementById('contenedor');
-
-//Dentro del div contenedor metí parrafo <p>
-contenedor.appendChild(parrafo);
-
-//borrar contendor
-contenedor.remove();
-
-console.log(parrafo);
+fetch('data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // Aquí puedes utilizar los datos como desees
+  })
+  .catch((error) => console.error('Error al cargar el archivo JSON:', error));
+/**
+ * .find
+ * .filter
+ * .map
+ * .sort
+ * .forEach
+ */
